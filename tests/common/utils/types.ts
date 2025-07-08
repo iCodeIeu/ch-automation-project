c5ba463eb71fbf6b81fa7f7453cd775743ba5ff8
Ieuan Hughes <iwjhughes@Ieuans-MacBook-Air.local>
2025-07-08 13:44:47 +0100

    chore: chap-17 added new type entry for EnquiryDetails

--- tests/common/utils/types.ts
+++ tests/common/utils/types.ts
@@ -19,3 +19,11 @@ export interface GuestBookingDetails {
   checkInDate: string;
   checkOutDate: string;
 }
+
+export interface EnquiryDetails {
+  name: string;
+  email: string;
+  phone: string;
+  subject: string;
+  message: string;
+}
