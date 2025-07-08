9427b53c72c5a00230ef9dd5e864327f3e58067b
Ieuan Hughes <iwjhughes@Ieuans-MacBook-Air.local>
2025-07-08 13:40:51 +0100

    chore: chap-14 added new constants for validation errors and typed existing ones

--- tests/common/utils/constants.ts
+++ tests/common/utils/constants.ts
@@ -1,2 +1,39 @@
-export const CLEANING_FEE = 25;
-export const SERVICE_FEE = 15;
+export const CLEANING_FEE: number = 25;
+export const SERVICE_FEE: number = 15;
+
+export const GUEST_DETAILS_FORM_ERRORS = {
+  EMPTY_SUBMISSION: [
+    'Firstname should not be blank',
+    'size must be between 3 and 18',
+    'must not be empty',
+    'size must be between 3 and 30',
+    'size must be between 11 and 21',
+    'must not be empty',
+    'Lastname should not be blank',
+  ],
+  POPULATED_SUBMISSION: [
+    'must be a well-formed email address',
+    'size must be between 3 and 30',
+    'size must be between 3 and 18',
+    'size must be between 11 and 21',
+  ],
+};
+
+export const ENQUIRY_DETAILS_FORM_ERRORS = {
+  EMPTY_SUBMISSION: [
+    'Subject must be between 5 and 100 characters.',
+    'Email may not be blank',
+    'Phone must be between 11 and 21 characters.',
+    'Phone may not be blank',
+    'Message must be between 20 and 2000 characters.',
+    'Subject may not be blank',
+    'Message may not be blank',
+    'Name may not be blank',
+  ],
+  POPULATED_SUBMISSION: [
+    'Message must be between 20 and 2000 characters.',
+    'Phone must be between 11 and 21 characters.',
+    'Subject must be between 5 and 100 characters.',
+    'must be a well-formed email address',
+  ],
+};
