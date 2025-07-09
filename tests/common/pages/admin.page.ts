6a1247e5471246f30479efdd341ebc1accda3b91
Ieuan Hughes <iwjhughes@Ieuans-MacBook-Air.local>
2025-07-09 15:36:29 +0100

    chore: chap-41 added new locators and helper for login via FE UI

--- tests/common/pages/admin.page.ts
+++ tests/common/pages/admin.page.ts
@@ -1,4 +1,5 @@
 import { Page, Locator } from '@playwright/test';
+import { validateAndPerform } from '../utils/shared-helpers';
 
 export class AdminPage {
   readonly page: Page;
@@ -6,6 +7,7 @@ export class AdminPage {
   readonly usernameInput: Locator;
   readonly passwordInput: Locator;
   readonly loginButton: Locator;
+  readonly invalidCredentialsError: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -13,5 +15,19 @@ export class AdminPage {
     this.usernameInput = page.getByRole('textbox', { name: 'Username' });
     this.passwordInput = page.getByRole('textbox', { name: 'Password' });
     this.loginButton = page.getByRole('button', { name: 'Login' });
+    this.invalidCredentialsError = page.locator('//*[@class="alert alert-danger"]');
+  }
+
+  /**
+   * Fills the username and password fields and clicks the login button.
+   * This method performs the login attempt but does not assert the outcome.
+   *
+   * @param username The username to input.
+   * @param password The password to input.
+   */
+  async login(username: string, password: string): Promise<void> {
+    await validateAndPerform(this.usernameInput).fill(username);
+    await validateAndPerform(this.passwordInput).fill(password);
+    await validateAndPerform(this.loginButton).click();
   }
 }
